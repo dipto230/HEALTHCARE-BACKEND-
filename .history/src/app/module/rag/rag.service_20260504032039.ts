371f8991d8f172c6d287b2d1a19f9f89345a8313
dipto230 <diptoroy230305@gmail.com>
2026-05-04 03:53:32 +0530

    get stats section also done

--- .history/src/app/module/rag/rag.service_20260504032039.ts
+++ .history/src/app/module/rag/rag.service_20260504032039.ts
@@ -115,33 +115,6 @@ export class RAGService {
     } catch (error) {
       console.log(error);
     }
-    }
-    
-    async getStats() {
-    try {
-      const totalDocuments = await prisma.$queryRaw(Prisma.sql`
-        SELECT COUNT(*) as count FROM "document_embeddings" WHERE "isDeleted" = false;
-        `);
-
-      const sourceTypeCounts = await prisma.$queryRaw(Prisma.sql`
-        SELECT "sourceType", COUNT(*) as count FROM "document_embeddings" WHERE "isDeleted" = false GROUP BY "sourceType"
-        `);
-
-      return {
-        totalActiveDocuments: Number((totalDocuments as any)[0]?.count ?? 0),
-        sourceTypeBreakdown: (sourceTypeCounts as any).reduce(
-          (acc: any, curr: any) => {
-            acc[curr.sourceType] = Number(curr.count);
-            return acc;
-          },
-          {},
-        ),
-        timestamp: new Date(),
-      };
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
   }
 
 
